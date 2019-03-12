21e1f5b96e71d51a2ddd077e0e8336ce7b32a2b1
Chitre <schitre@paypal.com>
2019-03-11 23:15:26 -0700

    component reuse and props

--- src/index.js
+++ src/index.js
@@ -2,13 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail/>
-            <CommentDetail/>
-            <CommentDetail/>
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure you want to do this?
+                </div>
+                
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    avatar={faker.image.avatar()} 
+                    author="Hermione" 
+                    timeAgo="Today at 4.48 pm" 
+                    content="Nice blog post! Informative~" 
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    avatar={faker.image.avatar()} 
+                    author="Harry" 
+                    timeAgo="Today at 1.00 am" 
+                    content="A good read."
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    avatar={faker.image.avatar()} 
+                    author="Ron" 
+                    timeAgo="Yesterday at 2.00 pm" 
+                    content="Interesting post!"
+                />
+            </ApprovalCard>
+            
         </div>
     );
 };
